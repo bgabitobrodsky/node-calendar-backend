4e2dec633e3963938812a7c4fe4bb0e5e7ea82b8
bgabitobrodsky <bernardogabito@gmail.com>
2023-02-24 15:22:25 -0300

    user creation in db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,30 @@
 const { response } = require('express')
+const User = require('../models/User');
 
-const createUser = ( req, res = response ) => {
+
+const createUser = async( req, res = response ) => {
 
     const { name, email, password } = req.body;
 
-    return res.status(201).json({
-        ok: true,
-        msg: 'register'
-    })
+    const user = new User( req.body );
+    
+    try {
+
+        await user.save(); 
+        
+        return res.status(201).json({
+            ok: true,
+            msg: 'register'
+        })
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        })
+    }
+
 }
 
 const loginUser = ( req, res = response ) => {
