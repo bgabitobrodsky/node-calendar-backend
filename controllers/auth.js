3d8f6f18bccca17e28a0b9c1dafc5db93d9624d9
bgabitobrodsky <bernardogabito@gmail.com>
2023-02-28 15:16:01 -0300

    login checking mail and password

--- controllers/auth.js
+++ controllers/auth.js
@@ -42,15 +42,43 @@ const createUser = async( req, res = response ) => {
 
 }
 
-const loginUser = ( req, res = response ) => {
+const loginUser = async( req, res = response ) => {
 
     const { email, password } = req.body;
     
+    try {
 
-    res.status(200).json({
-        ok: true,
-        msg: 'login'
-    })
+        const user = await User.findOne({ email });
+        if( !user ){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Email incorrecto'
+            });
+        }
+
+        const validPassword = bcrypt.compareSync( password, user.password );
+        if( !validPassword ){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Contraseña incorrecta'
+            })
+        }
+
+        return res.status(200).json({
+            ok: true,
+            uid: user.id,
+            name: user.name,
+        })
+
+
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        });
+    }
 }
 
 const renewToken = ( req, res = response ) => {
